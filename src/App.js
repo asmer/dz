8bb9804a40836604e80286503b1ba955f9e18d5c
asmer <asmer@asmer.org.ua>
2024-03-05 19:52:04 +0200

    якийсь приклад

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+const ReactLink = ({children="Learn React", a, b}) =>
+    <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        {children}<br/>
+        a + b = {a} + {b} = {a + b}<br/>
+        {Math.random()}
+    </a>
+
+// React.createElement('header', {className: 'App-header'})
+
+const Header = () => 
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <ReactLink a={1} b={2} />
+        <ReactLink a={3} b={20}>
+            Вчить React!!11111
+        </ReactLink>
+    </header>
+
+console.log(<h1>Заголовок</h1>)
+function App() {
+    return (
+        <div className="App">
+            <myComponent>Винахід нового тега HTML :-D</myComponent>
+            <div />
+            <input></input>
+            <br></br>
+            <Header />
+        </div>
+    );
 }
 
 export default App;
